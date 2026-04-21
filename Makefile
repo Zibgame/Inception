c4bb6e87139398a512f5b9d5b0701f4a0d05c186
Zibrian Cadinot <zcadinot@c2r5p5.42lehavre.fr>
2026-04-21 14:22:18 +0200

    Feat: add Docker compose and makefile

---
+++ Makefile
@@ -0,0 +1,10 @@
+all:
+	docker compose -f srcs/docker-compose.yml up --build
+
+down:
+	docker compose -f srcs/docker-compose.yml down
+
+re: down all
+
+clean:
+	docker system prune -af
--- requirements/mariadb/tools/init.sh
+++ requirements/mariadb/tools/init.sh
@@ -1,3 +1,14 @@
-docker build -t mariadb-test .
-docker run -d mariadb-test
-docker ps
+#!/bin/bash
+
+service mariadb start
+
+sleep 3
+
+mysql -e "CREATE DATABASE IF NOT EXISTS wordpress;"
+mysql -e "CREATE USER IF NOT EXISTS 'wpuser'@'%' IDENTIFIED BY 'wppass';"
+mysql -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'wpuser'@'%';"
+mysql -e "FLUSH PRIVILEGES;"
+
+mysqladmin shutdown
+
+exec mysqld_safe
