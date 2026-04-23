feaeca8f59c386f671ca8f3e28bf294632e89acf
Zibrian Cadinot <zcadinot@c1r4p13.42lehavre.fr>
2026-04-23 17:22:16 +0200

    Work !!

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -5,14 +5,18 @@ pid="$!"
 
 echo "Waiting for MariaDB..."
 
-while ! mysqladmin ping --silent; do
+until mysqladmin ping --silent; do
 	sleep 2
 done
 
-mysql -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
-mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
-mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
-mysql -e "FLUSH PRIVILEGES;"
+echo "MariaDB started"
+
+mysql -u root -p"$MYSQL_ROOT_PASSWORD" << EOF
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
+FLUSH PRIVILEGES;
+EOF
 
 wait "$pid"
